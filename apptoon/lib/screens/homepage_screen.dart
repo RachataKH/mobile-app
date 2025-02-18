d7856efbf5168a1fe6b795b9efbb7b521be736fd
RachataKH <Toon2574725@gmail.com>
2025-02-18 16:01:14 +0700

    01

--- apptoon/lib/screens/homepage_screen.dart
+++ apptoon/lib/screens/homepage_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
--- apptoon/lib/screens/menu_screen.dart
+++ apptoon/lib/screens/menu_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
   @override
   _SettingsScreenState createState() => _SettingsScreenState();
 }
